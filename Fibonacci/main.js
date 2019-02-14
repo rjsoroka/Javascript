a067740c988c21b03657276bda839dcb580f5841
RJ Soroka <sorokarj@gmail.com>
2019-02-14 04:35:57 -0600

    Added Fibonacci

---
+++ Fibonacci/main.js
@@ -0,0 +1,11 @@
+function fib(n) {
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return f;
+};
+
+document.write(fib(5));
